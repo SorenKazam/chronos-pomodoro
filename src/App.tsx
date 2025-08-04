edfa86a1065a2e623aedc916bb8fabdc5d1793f7
SorenKazam <tiago.guerreiro8957@gmail.com>
2025-08-04 16:37:13 +0100

    Organize components

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
 import "./styles/theme.css";
 import "./styles/global.css";
 
-export function App() {
-  console.log("Oi");
+import { Container } from "./components/Container";
+import { Heading } from "./components/Heading";
 
+export function App() {
   return (
     <>
-      <h1>EU SOU A APP</h1>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime
-        praesentium sint numquam est, ratione dolores doloribus recusandae qui,
-        illo eius veritatis, libero dolorem sit illum omnis dolor impedit ut
-        quas.
-      </p>
+      <Container>
+        <Heading>Chronos Pomodoro</Heading>
+      </Container>
     </>
   );
 }
