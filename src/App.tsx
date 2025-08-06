e6f90d702e5faa20fd36275f439fcbb2a86b2538
SorenKazam <tiago.guerreiro8957@gmail.com>
2025-08-06 18:47:16 +0100

    Organize imports in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import { Container } from "./components/Container";
-import { Logo } from "./components/Logo";
-
 import "./styles/theme.css";
 import "./styles/global.css";
+
+import { Container } from "./components/Container";
+import { Logo } from "./components/Logo";
 import { Menu } from "./components/Menu";
 import { CountDown } from "./components/CountDown";
 import { DefaultInput } from "./components/DefaultInput";
